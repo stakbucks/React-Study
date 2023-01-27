74744aa293747039d35830152ffd458caf9a0269
stakbucks <dhassidu11@gmail.com>
2023-01-27 20:39:22 +0900

    social-login--naver

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,15 +1,41 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
-import { REDIRECT_URI, REST_API_KEY } from "./LoginData";
+import {
+  naver_CLIENT_ID,
+  naver_REDIRECT_URI,
+  REDIRECT_URI,
+  REST_API_KEY,
+} from "./LoginData";
 
 const KakaoBtn = styled.img``;
+const NaverBtn = styled.img`
+  width: 300px;
+  height: 50px;
+`;
+
+const { naver } = window as any;
+export const naverLogin = new naver.LoginWithNaverId({
+  clientId: `${naver_CLIENT_ID}`,
+  callbackUrl: `${naver_REDIRECT_URI}`,
+  isPopup: false,
+  loginButton: { color: "green", type: 3, height: 58 },
+  callbackHandle: true,
+});
+
 function Login() {
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
   const handleLogin = () => {
     window.location.href = KAKAO_AUTH_URL;
   };
+
+  useEffect(() => {
+    naverLogin.init();
+  }, []);
+
   return (
     <div>
       <KakaoBtn src="login.png" onClick={handleLogin} />
+      <div id="naverIdLogin" />
     </div>
   );
 }
--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Home from "./Home";
 import LoggedIn from "./LoggedIn";
+import NaverLoggedIn from "./NaverLoggedIn";
 
 const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
       {
         path: "/loggedIn",
         element: <LoggedIn />,
+      },
+      {
+        path: "/naverLoggedIn",
+        element: <NaverLoggedIn />,
       },
     ],
   },
